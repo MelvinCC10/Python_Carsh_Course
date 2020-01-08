e18a1007c9930efd4bfbd4c5dc84868aa7e25c44
Reven <Melvin.cordon21@gmail.com>
2020-01-07 20:50:50 -0600

    Added a 2nd test for sampleFunction.py

--- Ch11 - Testing Code/test_code_example.py
+++ Ch11 - Testing Code/test_code_example.py
@@ -9,4 +9,9 @@ class NamesTestCase(unittest.TestCase):
         formatted_name = get_formatted_name('janis','joplin')
         self.assertEqual(formatted_name, 'Janis Joplin')
 
+    def test_first_last_middle_name(self):
+        """DO names like 'Robby john jackson' work?"""
+        formatted_name = get_formatted_name('Robby', 'john', 'jackson')
+        self.assertEqual(formatted_name,'Robby Jackson John')
+
 unittest.main()
